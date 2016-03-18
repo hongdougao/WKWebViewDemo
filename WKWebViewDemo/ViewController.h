cbe8a82d3320d381785d099af329e79f53e99ad8
Clair <yintai.com>
2016-03-18 17:02:45 +0800

    Initial Commit

---
+++ WKWebViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WKWebViewDemo
+//
+//  Created by yangyue on 16/3/18.
+//  Copyright © 2016年 com.yy.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WKWebViewDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WKWebViewDemo
+//
+//  Created by yangyue on 16/3/18.
+//  Copyright © 2016年 com.yy.www. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
